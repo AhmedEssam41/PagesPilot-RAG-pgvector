11670e6b85139d032b1164aa8e110d5e806b82bd
hesham <nbilha161@gmail.com>
2025-09-21 23:45:24 +0300

    fix(auth): check if admin already exist befor register

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -7,6 +7,9 @@ const JWT_SECRET = process.env.JWT_SECRET || "supersecret";
 const createAdmin = async (email: string, password: string) => {
   const hashed = await bcrypt.hash(password, 10);
 
+  //check if admin already exists
+  const admin = await prisma.admin.findUnique({ where: { email } });
+  if (admin) throw new Error("Admin already exists");
   return prisma.admin.create({
     data: { email, password: hashed },
   });
@@ -14,7 +17,7 @@ const createAdmin = async (email: string, password: string) => {
 
 const login = async (email: string, password: string) => {
   const admin = await prisma.admin.findUnique({ where: { email } });
-  if (!admin) throw new Error("Invalid credentials");
+  if (!admin) throw new Error("Admin not found");
   const valid = await bcrypt.compare(password, admin.password);
   if (!valid) throw new Error("Invalid credentials");
 
